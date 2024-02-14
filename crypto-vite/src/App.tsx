e8d828c5b20fb3580e12d836d858d64e3b7ae5ac
carroT200 <124577522+carroT200@users.noreply.github.com>
2024-02-14 15:20:00 +0400

    add axios

--- crypto-vite/src/App.tsx
+++ crypto-vite/src/App.tsx
@@ -1,24 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
+export type Tcrypto = {
+  id: string;
+  name: string;
+  current_price: number;
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [crypto, setCrypto] = useState<Tcrypto[]>();
+  useEffect(() => {
+    const url =
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en';
+    axios.get(url).then((response) => {
+      setCrypto(response.data);
+      console.log(response.data);
+    });
+  }, []);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        {crypto
+          ? crypto.map((item) => (
+              <p key={item.id}>
+                {item.name} - {item.current_price}
+              </p>
+            ))
+          : null}{' '}
+      </h1>
     </>
   );
 }
