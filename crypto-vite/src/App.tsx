d7f33204b86133d1048614ce94351d5d2cb4e71f
carroT200 <124577522+carroT200@users.noreply.github.com>
2024-02-19 15:36:30 +0400

    add router dom

--- crypto-vite/src/App.tsx
+++ crypto-vite/src/App.tsx
@@ -1,36 +1,9 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
-import './App.css';
-
-export type Tcrypto = {
-  id: string;
-  name: string;
-  current_price: number;
-};
+import Header from './componets/Header/Header';
 
 function App() {
-  const [crypto, setCrypto] = useState<Tcrypto[]>();
-  useEffect(() => {
-    const url =
-      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en';
-    axios.get(url).then((response) => {
-      setCrypto(response.data);
-      console.log(response.data);
-    });
-  }, []);
-
   return (
     <>
-      <h1>
-        {crypto
-          ? crypto.map((item) => (
-              <p key={item.id}>
-                {item.name} - {item.current_price}
-              </p>
-            ))
-          : null}{' '}
-      </h1>
+      <Header />
     </>
   );
 }
---
+++ crypto-vite/src/componets/Header/Header.tsx
@@ -0,0 +1,7 @@
+import styles from '../Header/Header.module.css';
+
+const Header = () => {
+  return <div className={styles.header}>dsfs</div>;
+};
+
+export default Header;
